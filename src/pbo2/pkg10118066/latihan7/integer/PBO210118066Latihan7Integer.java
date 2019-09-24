aceefae87cfca76a002ee3694f6e7f086c40bf57
Tegar Lucky'Q Oakley <55655976+tegaroakley@users.noreply.github.com>
2019-09-24 15:55:00 +0900

    Add files via upload

---
+++ src/pbo2/pkg10118066/latihan7/integer/PBO210118066Latihan7Integer.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118066.latihan7.integer;
+
+/**
+ *
+ * @author 
+ * NAMA  : Tegar Lucky'Q Oakley 
+ * NIM   : 10118066 
+ * KELAS : IF2 
+ * Deskripsi Program : Program ini berisi program untuk Menampilkan Integer
+ */
+public class PBO210118066Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte\t : " + b);
+        System.out.println("short\t : " + s);
+        System.out.println("integer\t : "  + i);
+        System.out.println("long\t : " + l);
+    }
+    
+}
